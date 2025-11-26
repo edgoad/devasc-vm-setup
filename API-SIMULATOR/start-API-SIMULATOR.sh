f07a603af9b21688d7be9bb00b9f7cbd99aeff21
edgoad <edgoad@gmail.com>
2025-11-26 10:52:32 -0800

    update chromium

--- API-SIMULATOR/start-API-SIMULATOR.sh
+++ API-SIMULATOR/start-API-SIMULATOR.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-iptables -t nat -I OUTPUT -p tcp -d 192.0.2.1 --dport 80 -j DNAT --to-destination 192.0.2.1:59980
+/usr/sbin/iptables -t nat -I OUTPUT -p tcp -d 192.0.2.1 --dport 80 -j DNAT --to-destination 192.0.2.1:59980
 cd /opt/API-SIMULATOR
 /opt/API-SIMULATOR/venv/bin/python api-simulator.py  > /dev/null 2>&1
